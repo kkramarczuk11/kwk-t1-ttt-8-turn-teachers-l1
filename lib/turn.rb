2e51abf05370cb403ddfaf2a941cc15f95425f47
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-05-16 23:59:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
   move(board, index, "O")
-else
+else puts "invalid"
+  input = gets.strip
   end
   display_board(board)
 end
